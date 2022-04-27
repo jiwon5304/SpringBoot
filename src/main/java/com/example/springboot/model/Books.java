70650b3f114a02ebc340d95654b8853c8e9d68dd
jiwon5304 <80395324+jiwon5304@users.noreply.github.com>
2022-04-27 18:20:17 +0900

    user,book 테이블생성

---
+++ src/main/java/com/example/springboot/model/Books.java
@@ -0,0 +1,16 @@
+package com.example.springboot.model;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Books {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "book_title",nullable = false, length = 100)
+    private String title;
+
+    @Column(name = "book_category",nullable = false, length = 100)
+    private String category;
+}
