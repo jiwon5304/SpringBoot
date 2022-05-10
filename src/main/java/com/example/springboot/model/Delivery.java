8f746796c5fbb174a94021c65835510bee0c5b97
jiwon5304 <80395324+jiwon5304@users.noreply.github.com>
2022-05-10 16:36:24 +0900

    회원 도메인

--- src/main/java/com/example/springboot/model/Delivery.java
+++ src/main/java/com/example/springboot/model/Delivery.java
@@ -1,7 +1,6 @@
 package com.example.springboot.model;
 
 import lombok.*;
-
 import javax.persistence.Entity;
 import javax.persistence.*;
 
--- src/main/java/com/example/springboot/model/item/Item.java
+++ src/main/java/com/example/springboot/model/item/Item.java
@@ -10,6 +10,7 @@ import java.util.List;
 @Entity
 // 상속관계 매핑 전략(JOINED / SINGLE_TABLE / TABLE_PER_CLASS)
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+// 부모 클래스에 선언
 @DiscriminatorColumn(name = "dtype")
 @Getter @Setter
 public abstract class Item {
@@ -26,6 +27,7 @@ public abstract class Item {
 
     // Item 에서 OrderItem 을 조회할 필요가 없으므로 관계 지정하지 않음.
 
+    // 실무에서는 ManyToMany 를 사용하지 않도록 한다.
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<Category>();
 }
