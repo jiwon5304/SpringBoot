fcea7753e8485b3dd17cdd30841649292a6a3cc7
jiwon5304 <80395324+jiwon5304@users.noreply.github.com>
2022-05-12 14:28:40 +0900

    주문 도메인

--- src/main/java/com/example/springboot/service/OrderService.java
+++ src/main/java/com/example/springboot/service/OrderService.java
@@ -35,15 +35,23 @@ public class OrderService {
         // 주문상품 생성
         OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
 
+        // 아래와 같이도 생성이 가능하나 유지보수가 어려워짐
+        // OrderItem 모델에서 @NoArgsConstructor 로 객체 생성 제약
+        // OrderItem.OrderItem1 = new OrderItem();
+        // orderItem1.setCount();
+
         // 주문 생성
         Order order = Order.createOrder(member, delivery, orderItem);
 
-        // 주문 저장
+        // Order 모델에서 @NoArgsConstructor 로 새로운 객체 생성을 막음
+        //new Order();
+
+        // 주문 저장 : cascade 옵션으로 orderItem, delivery 는 동시에 저장됨
         orderRepository.save(order);
         return order.getId();
     }
 
-        // 주문 취소
+    // 주문 취소
     @Transactional
     public void cancelOrder(Long orderId) {
 
@@ -53,4 +61,7 @@ public class OrderService {
         // 주문 취소
         order.cancel();
     }
+
+
+
 }
